2c00603c8c8de724d1ef6ce5368f7f67fb53542a
captainozlem <ozlemniksch@gmail.com>
2019-04-16 19:20:05 -0400

    logo changed

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>CHRR BLSSM</h1>
+    <h1>YOUR NEXT TRIP</h1>
     <nav>
       {isLoggedIn ? (
         <div>
